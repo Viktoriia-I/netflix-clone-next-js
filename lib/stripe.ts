fd38c2315596e34b0b88b4dc872928412cc4f87f
Viktoriia Ivankova <viktoriia.i@code-care.pro>
2022-08-20 18:16:50 +0300

    Add account page

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,24 +1,35 @@
-import {
-  createCheckoutSession,
-  getStripePayments,
-} from '@stripe/firestore-stripe-payments'
-import { getFunctions, httpsCallable } from '@firebase/functions'
-import app from '../firebase'
+import { createCheckoutSession, getStripePayments } from '@stripe/firestore-stripe-payments';
+import { getFunctions, httpsCallable } from '@firebase/functions';
+import app from '../firebase';
 
 const payments = getStripePayments(app, {
   productsCollection: 'products',
-  customersCollection: 'customers'
-})
+  customersCollection: 'customers',
+});
 
 const loadCheck = async (priceId: string) => {
   await createCheckoutSession(payments, {
     price: priceId,
     success_url: window.location.origin,
-    cancel_url: window.location.origin
+    cancel_url: window.location.origin,
   })
-  .then(snapshot => window.location.assign(snapshot.url))
-  .catch(error => console.log(error))
-}
+    .then((snapshot) => window.location.assign(snapshot.url))
+    .catch((error) => console.log(error));
+};
 
-export default payments
-export { loadCheck }
+const goToBillingPortal = async () => {
+  const instance = getFunctions(app, 'us-central1');
+  const functionReference = httpsCallable(
+    instance,
+    'ext-firestore-stripe-payments-createPortalLink'
+  );
+
+  await functionReference({
+    returnUrl: `${window.location.origin}/account`,
+  })
+    .then(({ data }: any) => window.location.assign(data.url))
+    .catch((error) => console.log(error.message));
+};
+
+export default payments;
+export { loadCheck, goToBillingPortal };
